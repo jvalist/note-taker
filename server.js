ee74d37e8f453d934171662f5813df32a68e2c88
Johnny Velasco <johnnyvelasco@Johnnys-MacBook-Air.local>
2023-01-31 23:26:01 -0800

    second sommit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const  fs  = require('fs');
 const path = require('path');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const notes=require("./db/db.json")
 const app = express();
 
